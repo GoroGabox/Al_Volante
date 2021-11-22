23419e09aef79a970d18636c0a0be16aea341834
Gabriel Díaz <alfons.diaz97@gmail.com>
2021-11-22 18:43:40 -0300

    WhatsApp Button
    se agrego el boton de whatsApp, el cual se conecto a la WA API, falta agregar animaciones

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navLinks = document.getElementsByClassName('nav-links')[0]
+const waButton = document.getElementsByClassName('whatsApp_Button-icon')[0]
+const waUpper = document.getElementsByClassName('whatsApp_Button-upper')[0]
+const waCallToAction = document.getElementsByClassName('whatsApp_Button-callToAction')[0]
 
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active')
+})
+
+waButton.addEventListener('click', () => {
+    waUpper.classList.toggle('active')
+    waCallToAction.classList.toggle('active')
 })
