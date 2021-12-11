de4edffd5ed46412269427d36a6e51d79e4996fd
Gabriel Díaz <alfons.diaz97@gmail.com>
2021-12-10 23:32:21 -0300

    Boton whatsapp terminado
    animacion y actualizacion del componente. Primera vez que pienso en componentes.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+//Navigation Menu Collapse
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navLinks = document.getElementsByClassName('nav-links')[0]
+
+//WhatsApp Button
 const waButton = document.getElementsByClassName('whatsApp_Button-icon')[0]
 const waUpper = document.getElementsByClassName('whatsApp_Button-upper')[0]
 const waCallToAction = document.getElementsByClassName('whatsApp_Button-callToAction')[0]
+const waIcon = document.getElementsByClassName('wa_icon')[0]
+const xIcon = document.getElementsByClassName('x_icon')[0]
 
+//functions
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active')
 })
@@ -11,4 +17,6 @@ toggleButton.addEventListener('click', () => {
 waButton.addEventListener('click', () => {
     waUpper.classList.toggle('active')
     waCallToAction.classList.toggle('active')
+    waIcon.classList.toggle('active')
+    xIcon.classList.toggle('active')
 })
